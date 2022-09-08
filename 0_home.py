b5cac82153a5d09e47bbe67b280fc3e6c73b47eb
Bruno Coelho <brunoc510@hotmail.com>
2022-09-08 15:31:48 -0300

    Up_08_09 defindo estrutura e quem seguira com o estudo.

---
+++ 0_home.py
@@ -0,0 +1,27 @@
+# boas vindas
+
+# 0 - Menu
+
+#Digite a opção desejada:
+#
+#1 - 
+#2 - 
+#3 - 
+#4 - 
+
+
+
+# Variaveis - (steep 1 )
+
+
+
+# 1 - Cadastro - Julia - Bruno ( 2)
+
+
+
+# 2 - Vendas - Fernando - ( 2 )
+
+
+# 3 - Relatorio ( step 3 )
+
+# 4 - Sair
