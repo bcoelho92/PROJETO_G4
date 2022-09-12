48bf55a295ba4a2354919f36259e8a75ac29920b
julia <48657987+0xjulia@users.noreply.github.com>
2022-09-12 00:29:43 -0300

    Menu de cadastro

--- 1_Cadastro.py
+++ 1_Cadastro.py
@@ -14,11 +14,43 @@ def clear_console():
 
 clear_console() 
 
+print (''' 
 
-print("Você está no Menu de Cadastro de Produto! Digite Q para voltar ao menu anterior \n")
+Bem vindo(a) ao Menu de Estoque! Digite a opção que você deseja:
+
+1 - Cadastrar um novo produto
+2 - Listar todos os Produtos Cadastrados 
+3 - Apagar um Produto do Cadastro
+4 - Sair
+''')
+print('')
+
+# variaveis 
+
+menu = int(input('Digite a opção desejada: '))
+
+while menu >=5 or menu <=0:
+    menu = int(input('Digite a opção válida: '))
+
+if menu == 1:
+    clear_console()
+    print("Você deseja cadastrar um produto no estoque? Para retroceder, digite Q \n")
+
+elif menu == 2:
+    clear_console()
+    print("Esses são os produtos cadastrados no estoque:")
+
+elif menu == 3:
+    clear_console()
+    print("Você deseja apagar um produto do estoque? Para retroceder, digite Q \n")
+
+elif menu == 4:
+    clear_console()
+    print("Saindo do Menu de Cadastro\n")
+
 
 lista = {} #criando lista vazia 
-qtd = int(input("Quantos produtos você gostaria de adicionar? ")) 
+qtd = int(input("\nQuantos produtos você gostaria de adicionar? ")) 
 
 for i in range(qtd):
     produto = input("Digite o nome do produto: ")
@@ -31,7 +63,7 @@ for produto in lista:
     print("-", (produto), "R$",(valor))
 
 #remover item cadastrado
-deleta = input("Qual produto você gostaria de deletar? Aperte ENTER para reexibir a lista: ")
+deleta = input("Qual produto você gostaria de deletar? Digite ENTER para reexibir a lista: ")
 if deleta in lista:
     lista.pop(deleta)
     print ("\nVocê apagou o item: \n-", (deleta), "\n")
