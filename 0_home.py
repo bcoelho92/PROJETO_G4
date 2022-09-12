71d7dcc2d62b8b32192ee4bcc9ab9283ffefae8c
Bruno Coelho <brunoc510@hotmail.com>
2022-09-12 13:03:42 -0300

    Up Menu

--- 0_home.py
+++ 0_home.py
@@ -8,55 +8,59 @@
 ####################################################################################
 
 # Boas vindas / 0 - Menu
+
+''''''
 print()
 nomeCliente = str(input('Digite sue nome: '))
+Menu = 0
 
-def main():
-    while True:
-        print('''
+print('''
 ***********************************************************
  Bem vindo(a) {} ao sistema de vendas da Organico’s! 
                                                      
- Digute o numero da opção que você deseja:           
+ Essas são as opções do programa:           
                                                      
  [1] - Cadastro                                 
  [2] - Vendas                                   
  [3] - Relatório                                
- [4] - Sair                                     
+ [4] - Sair                                     '
                                                      
 ***********************************************************
 '''.format(nomeCliente))
     
-        menu = int(input('Digite a opção: '))
+def main():
+    while True:
+        menu = int(input('{}, digite a opção: '.format(nomeCliente)))
         print()
         
         if menu == 1:
-            print ('Cadastro')
+            print ('Menu Cadastro')
+
+            #   1 - Menu de Cadastro - Julia 
+            #   Cadastramento de produtos
+            #   *Listar produtos cadastrados*
+            #   Deleção de produtos
+
         elif menu == 2:
-            print ('Vendas')
+            print ('Menu Vendas')
+
+            #   2 - Menu de Vendas - Fernando 
+            #   Adição de produtos ao carrinho
+            #   Finalização da venda do carrinho (dizendo quanto o usuário tem que pagar)
+
         elif menu == 3:
-            print ('Relatorio ')
+            print ('Menu Relatorio ')
+
+            #   3 - Menu Relatorio 
+            #   Extrato de produtos vendidos
+
         elif menu == 4:
             print('fim do programa')
             break
+
         else: 
             print('Digite a opção valida: ')
             main()
+            # 4 - Sair 
+
 main() 
-
-
-# 1 - Menu de Cadastro - Julia - Bruno (Steep 1 )
-
-#   Cadastramento de produtos
-#   *Listar produtos cadastrados*
-#   Deleção de produtos
-
-# 2 - Menu de Vendas - Fernando - Tiago (Steep 2)
-#   Adição de produtos ao carrinho
-#   Finalização da venda do carrinho (dizendo quanto o usuário tem que pagar)
-
-# 3 - Relatorio ( steep 4 )
-#   Extrato de produtos vendidos
-
-# 4 - Sair (Steep 3 )
-
