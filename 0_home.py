d30d98c5aaa813c79aa1210120860a6bd7a2029a
Bruno Coelho <brunoc510@hotmail.com>
2022-09-14 11:11:35 -0300

    Up estrutura

--- 0_home.py
+++ 0_home.py
@@ -124,141 +124,22 @@ def main():
                 if menuVendas ==1:
                     print('Adição de produtos ao carrinho')
                     clear()
-                    print("Veja a seguir nosso catálogo de produtos: \n")
-                    index = 0 #Referência para posição
-                    qtdProdutos = len(produtos)
-                    while qtdProdutos > 0: #Print: lista de produtos
-                        print(list(produtos)[index].title())
-                        print("Preço: R$",float(list(produtos.values())[index]))
-                        print("\n")
-                        index = index +1
-                        qtdProdutos = qtdProdutos -1
                     
-                    escolha = input("Informe o produto que deseja adicionar ao carrinho: ")
-                    while str(escolha).lower() not in produtos: #check LOWER!
-                        print("\nProduto inválido.")
-                        escolha = input("Informe um nome válido: ")  
-                        escolha = escolha.lower() #Produto escolhido definido.
-                    qtdEscolha = (input("Informe a quantidade desejada: "))
-
-                    while str(qtdEscolha).isnumeric() is not True or int(qtdEscolha) < 1:#check
-                        print("\nQuantidade inválida.")
-                        qtdEscolha = (input("Informe uma quantidade válida: "))
-                    qtdEscolha = int(qtdEscolha) #Quantidade escolhida definida.
-                    
-                    carrinho[escolha] = qtdEscolha #Adiciona ao carrinho
-                    carrinhoAdicionar = input("\nDeseja adicionar mais produtos ao carrinho? Digite SIM ou NÃO: ")
-                    while carrinhoAdicionar.lower() != "sim" and carrinhoAdicionar.lower() != "não": #check
-                        print("\nResposta inválida.")
-                        carrinhoAdicionar = input("Digite SIM ou NÃO: ")
-                    print("")
-
-                    if carrinhoAdicionar.lower() == "sim":
-                        menuVendas = 1
-                        clear()
-                    elif carrinhoAdicionar.lower() == "não":
-                        menuVendas = 3
-                        clear()  
-
                 elif menuVendas == 2:
                     print ('Remnmover produtos do Carrinho. ')
                     clear()
-                    print("CARRINHO") #Print carrinho
-                    countCarrinho = len(carrinho)
-                    index = 0
-                    total_local = 0
-                    while countCarrinho > 0:
-                        print("\n")
-                        print((list(carrinho)[index]).title())
-                        print("Quantidade:", list(carrinho.values())[index])
-                        print("Preço: R$", float(produtos[list(carrinho)[index]]))
-                        countCarrinho = countCarrinho -1
-                        total_local = total_local + produtos[list(carrinho)[index]]
-                        index = index +1
-                    print("\n Total a pagar: R$", float(total_local))#>
-
-                    prodRemovido = input("\nInforme o nome do produto que deseja remover: ")
-                    while str(prodRemovido).lower() not in carrinho: #check LOWER?
-                            print("\nProduto inválido.")
-                            prodRemovido = input("Informe um nome válido: ")
-
-                    carrinho.pop(f'{prodRemovido}')
-                    clear()
-                    print(prodRemovido.title(), "removido com sucesso!") #Removido
-                        
-                    questaoRemover = input("\nDeseja remover outro produto? Digite SIM ou NÃO: ") #Repete?
-                    while questaoRemover.lower() != "sim" and questaoRemover.lower() != "não":#check
-                            print("\nResposta inválida.")
-                            questaoRemover = input("Digite SIM ou NÃO: ")
-
-                    if questaoRemover.lower() == "sim":
-                        menuVendas = 2
-
-                        if total_local == 0:
-                            print("\nSeu carrinho está vazio.")
-                            enter = input("Aperte Enter para voltar ao início.")
-                            menuVendas = 0
-                    elif questaoRemover.lower() == "não":
-                        menuVendas = 3   
-                                         
+                                            
                 elif menuVendas == 3:
                     print ('Finalização da venda do carrinho')
                     clear()
-                    print("CARRINHO") #Print carrinho
-                    countCarrinho = len(carrinho)
-                    index = 0
-                    total_local = 0
-                    while countCarrinho > 0:
-                        print("\n")
-                        print((list(carrinho)[index]).title())
-                        print("Quantidade:", list(carrinho.values())[index])
-                        print("Preço: R$", float(produtos[list(carrinho)[index]]))
-                        countCarrinho = countCarrinho -1
-                        total_local = total_local + produtos[list(carrinho)[index]]
-                        index = index +1
-                    print("\n Total a pagar: R$", float(total_local))#>
-
-                    print(descm2)
-                    menuVendas = input("\nDigite a opção desejada: ")
-                    if str(menuVendas) not in "1230":#check
-                        print("Opção inválida!")
-                        menuVendas = input("Digite novamente: ")
-
-                    if int(menuVendas) == 3 and total_local != 0: #Finalizar compra
-                        clear()
-                        print("CARRINHO") #Print carrinho
-                        countCarrinho = len(carrinho)
-                        index = 0
-                        total_local = 0
-                        while countCarrinho > 0:
-                            print("\n")
-                            print((list(carrinho)[index]).title())
-                            print("Quantidade:", list(carrinho.values())[index])
-                            print("Preço: R$", float(produtos[list(carrinho)[index]]))
-                            countCarrinho = countCarrinho -1
-                            total_local = total_local + produtos[list(carrinho)[index]]
-                            index = index +1
-                        print("\n Total a pagar: R$", float(total_local))#>
-
-                        enter = input("\nAperte Enter para confirmar a compra")
-                        clear()
-                        print("Compra efetuada com sucesso!")
-                        enter = input("\nAperte Enter para voltar ao menu inicial")
-                        carrinho = {}
-                        menuVendas = 0
-                    
-                    if total_local == 0:
-                        print("\nSeu carrinho está vazio!")
-                        enter = input("Aperte Enter para voltar")
-                        menuVendas = 0
+                  
                 elif menuVendas == 4:
                     break
                 elif menuVendas == 5:
                     print ('Fim do programa')
                     sys.exit() 
                 else: 
-                    print('Opção invalida, digite uma opção valida: ')
-                    
+                    print('Opção invalida, digite uma opção valida: ')                  
         elif menuGeral == 3:
             print (descm3)
             print()
